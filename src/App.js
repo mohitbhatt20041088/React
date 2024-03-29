ca3117b397eae2a55c1031d3bd084e05a89aff6a
mohitbhatt20041088 <120396294+mohitbhatt20041088@users.noreply.github.com>
2024-03-29 23:28:07 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
+import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField:''
+
+    };
+    console.log('constructor');
+  }
+
+  componentDidMount(){
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then((users) => this.setState({ monsters: users }, () => {
+        console.log(this.state);
+      }));
+  }
+
+  render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input className='search-box' type='search' placeholder='search monster' onChange={(event) => {
+          const searchField=event.target.value.toLowerCase();
+          this.setState({ searchField });
+        }}/>
+        {
+          filteredMonsters.map((monster) => {
+            return (
+              <div key={monster.id}>
+                <h1 style={{ color: 'black', fontSize: '24px', fontFamily: 'fantasy' }}>{monster.name}</h1>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
